869654d5f6b4120b7c622a486399de7ff5ef82b6
jonmateo <compromiseddoctrines@gmail.com>
2020-10-21 10:02:43 +0800

    updated module 3

--- src/index.js
+++ src/index.js
@@ -1,45 +1,7 @@
-import './SeasonDisplay.css';
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
-
-class App extends React.Component{
-
-    state = { lat: null, errorMessage: '' };
-
-    componentDidMount() {
-       window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat: position.coords.latitude}),
-            (err) => this.setState({ errorMessage: err.message })
-        );
-    }
-
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat){
-            return <div>Error: {this.state.errorMessage}</div>
-       }
-
-       if (!this.state.errorMessage && this.state.lat){
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        return <div><Spinner message="Please accept location request"/></div>;
-    }
-
-    // componentDidUpdate() {
-    //     console.log("Component updated!");
-    // }
-
-    render() {
-       return(
-        <div className="border red">
-            {this.renderContent()}
-        </div>
-       );
-    }
-}
+import App from './components/App';
 
 ReactDOM.render(
-    <App />, document.querySelector("#root")
-);
+    <App />, document.querySelector('#root')
+)
