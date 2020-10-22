83b69769e59efcd99419da7f3c8346c4f1f55d07
Bon <compromiseddoctrines@gmail.com>
2020-10-22 13:32:31 +1200

    completed module four

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID onFUPdeFBPSwKj_XfGi7pQolvW_Mi062bY3M5V_VSgs'
-    }
-});
-
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
-    state = {
-        term: ''
+    state = { term: '' };
+
+    onInputChange = (e) => {
+        this.setState({
+            term: e.target.value
+        })
     };
 
     onFormSubmit = (e) => {
         e.preventDefault();
 
-        this.props.onSubmit(this.state.term);
-    }
+        this.props.onFormSubmit(this.state.term);
+    };
 
     render(){
-        return (
-            <div className="ui segment">
-                <form className="ui form" onSubmit={this.onFormSubmit}>
+        return(
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label>Image Search</label>
+                        <label>
+                            Video Search
+                        </label>
                         <input 
                             type="text" 
                             value={this.state.term}
-                            onChange={(e) => this.setState({term: e.target.value})} 
+                            onChange={this.onInputChange}
                         />
                     </div>
                 </form>
