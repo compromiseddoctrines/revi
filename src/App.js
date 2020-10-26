8b84dfa604b60c11f978b9fbf4d8c2dd064d80be
jonmateo <compromiseddoctrines@gmail.com>
2020-10-26 19:35:26 +0800

    finished module five using hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -32,7 +34,8 @@ const options = [
         label: 'A shade of Blue',
         value: 'blue'
     }
-]
+];
+
 
 export default () => {
 
@@ -40,7 +43,24 @@ export default () => {
 
     return(
         <div>
-            <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     )
 }
